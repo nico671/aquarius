acedfd00891869c4f7bea32c81e6abe6ed56cf15
nico671 <68139913+nico671@users.noreply.github.com>
2023-07-04 11:56:01 -0400

    d

--- tweak/GRPCell.h
+++
@@ -1,9 +0,0 @@
-#import "GRPNotifDataGroups.h"
-#import "GcUniversal/GcImageUtils.h"
-@interface GRPCell : UICollectionViewCell
-@property (nonatomic,retain) UIColor *tintColor;
-@property (nonatomic,retain) GRPNotifDataGroups *group;
-@property (nonatomic,retain) UIImageView *iconView;
-@property (nonatomic,retain) UILabel *countLabel;
-+ (CGSize) cellSize ;
-@end
